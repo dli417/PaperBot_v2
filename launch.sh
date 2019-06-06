449ba381a2b06e198576adb4e667fa387da04ce0
Patricia Maraver <pmaraver@masonlive.gmu.edu>
2019-06-06 04:56:31 +0200

    v2

---
+++ launch.sh
@@ -0,0 +1,6 @@
+nohup java -jar ./Articles-1.0.jar &
+nohup java -jar ./CrossRef-1.0.jar &
+nohup java -jar ./Gateway-1.0.jar &
+nohup java -jar ./Metadata-1.0.jar &
+nohup java -jar ./PubMed-1.0.jar &
+nohup java -jar ./Search-1.0.jar &
